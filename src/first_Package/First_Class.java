7ce024affd56757b1ed2dcf8395a825a63cbcc6b
Cebbet <114309229+Cebbet@users.noreply.github.com>
2023-01-07 14:33:06 -0500

    Update First_Class.java

--- src/first_Package/First_Class.java
+++ src/first_Package/First_Class.java
@@ -5,5 +5,12 @@ public class First_Class {
     public static void main(String[] args) {
 
         System.out.println("First Class");
+        
+        update
+            
+            System.out.println("First Class");
+        
+        
+     
     }
 }
